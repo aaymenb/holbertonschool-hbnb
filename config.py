b0558cfe73fab87a84d85248b95afc54fda0a7eb
aaymenb <9954@holbertonstudents.com>
2025-03-02 00:41:36 +0100

    HBNB prt2

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-.
+class Config:
+    SECRET_KEY = 'maclésecrete3009'
+    FLASK_ENV = 'development'        # Environnement de développement
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'  # Base de données SQLite (pour la partie 3)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Désactive le suivi des modifications non suivies
+
