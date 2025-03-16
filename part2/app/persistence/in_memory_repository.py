574de6a3a99ff50483263716cd6f77d016e50066
othmanmez <9796@holbertonstudents.com>
2025-03-16 15:42:22 +0100

    Delete part2 directory

--- part2/app/persistence/in_memory_repository.py
+++
@@ -1,9 +0,0 @@
-class InMemoryRepository:
-    def __init__(self):
-        self.storage = {}  # Dictionnaire pour stocker les utilisateurs en mémoire
-
-    def add(self, user):
-        self.storage[user.id] = user
-
-    def get(self, user_id):
-        return self.storage.get(user_id)
